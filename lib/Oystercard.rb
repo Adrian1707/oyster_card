eb5d10c3355fd5a541e3aa6c33b092d60bb8c0e8
Adrian Booth <Adrian.booth17@gmail.com>
2015-11-26 23:59:43 +0000

    Cannot top up more then £90

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -2,8 +2,19 @@ class Oystercard
 
   attr_reader :balance
 
+  MAXIMUM_BALANCE = 90
+
   def initialize
     @balance = 0
+    @max_balance = MAXIMUM_BALANCE
+  end
+
+  def top_up(amount)
+    if @balance + amount > @max_balance
+       raise "Cannot have a balance of more then £#{@max_balance}"
+    else
+    @balance += amount
+    end
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,14 @@ describe Oystercard do
     expect(subject.balance).to eq(0)
   end
 
+  it 'can be topped up' do
+    subject.top_up(5)
+    expect(subject.balance).to eq(5)
+  end
+
+  it 'raises error is customer tries to top up more then £90' do
+    subject.top_up(80)
+    expect{subject.top_up(11)}.to raise_error "Cannot have a balance of more then £90"
+  end
+
 end
