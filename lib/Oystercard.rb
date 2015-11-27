85ed2849cec8538b11964b0fa8efc1410037d750
Adrian Booth <Adrian.booth17@gmail.com>
2015-11-27 21:29:48 +0000

    refactor

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -3,10 +3,12 @@ class Oystercard
   attr_reader :balance, :in_journey
 
   MAXIMUM_BALANCE = 90
+  MINIMUM_BALANCE = 2
 
   def initialize
     @balance = 0
     @max_balance = MAXIMUM_BALANCE
+    @min_balance = MINIMUM_BALANCE
     @in_journey = false
   end
 
@@ -20,11 +22,12 @@ class Oystercard
   end
 
   def touch_in
-    raise "Cannot touch in with no balance" if @balance == 0
+    raise "Cannot touch in with balance less then £2" if @balance < @min_balance
     @in_journey = true
   end
 
   def touch_out
+    raise "Cannot top out if not in journey" if @in_journey = false
     @in_journey = false
     deduct(2)
   end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,12 +11,6 @@ describe Oystercard do
     expect(subject.balance).to eq(5)
   end
 
-  it 'raises error is customer tries to top up more then £90' do
-    maximum_balance = Oystercard::MAXIMUM_BALANCE
-    subject.top_up(maximum_balance)
-    expect{subject.top_up(1)}.to raise_error "Cannot have a balance of more then £90"
-  end
-
   it 'reduces the balance when deduct method is called' do
     subject.top_up(5)
     subject.deduct(1)
@@ -29,23 +23,39 @@ describe Oystercard do
     expect(subject.in_journey).to eq(true)
   end
 
+  it 'raises error is customer tries to top up more then £90' do
+    maximum_balance = Oystercard::MAXIMUM_BALANCE
+    subject.top_up(maximum_balance)
+    expect{subject.top_up(1)}.to raise_error "Cannot have a balance of more then £90"
+  end
+
   context 'when touches out' do
+
     before do
       subject.top_up(5)
       subject.touch_in
       subject.touch_out
     end
+
     it 'in_journey is set to false' do
       expect(subject.in_journey).to eq(false)
     end
 
-    it 'deducts balance of £2 when card touches out' do
+    it 'deducts balance of £2' do
       expect(subject.balance).to eq(3)
     end
+
   end
 
   it 'cannot touch in if balance is 0' do
-    expect{subject.touch_in}.to raise_error "Cannot touch in with no balance"
+    minimum_balance = Oystercard::MINIMUM_BALANCE
+    subject.top_up(minimum_balance-1)
+    expect{subject.touch_in}.to raise_error "Cannot touch in with balance less then £2"
+  end
+
+  it 'cannot touch out if not in journey' do
+    subject.top_up(5)
+    expect{subject.touch_out}.to raise_error "Cannot top out if not in journey"
   end
 
 
